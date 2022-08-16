73a440e5714940386f51eccb4c3796d17be42159
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-08-17 01:18:15 +0300

    navbar styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,14 +8,24 @@ const Navbar = () => {
     <div className='navbar'>
         <div className='container navbar-content'>
             <div className='navbar-left'>
-                <MenuIcon className='navbar-burger' />
-                <img className='my-navbar-photo' src='https://picsum.photos/200/300' />
-                <div>
-                    <span>Doğukan</span>
-                    <span> Topçu</span>
+                {/* <MenuIcon className='navbar-burger' /> */}
+                <div className="navbar-burger">
+                  <div className='burger-line line1' />
+                  <div className='burger-line line2' />
+                  <div className='burger-line line3' />
+                </div>
+                <div className='navbar-fullName'>
+                    Doğukan Topçu
                 </div>
             </div>
-            <div className='navbar-right'></div>
+            <div className='navbar-right'>
+              <div className="navbar-links">
+                <div className='nav-link'>Blogs</div>
+                <div className='nav-link'>Portfolio</div>
+                <div className='nav-link'>Discord</div>
+              </div>
+                <img className='my-navbar-photo' src='https://picsum.photos/200/300' />
+            </div>
         </div>
     </div>
   )
