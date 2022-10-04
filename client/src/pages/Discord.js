825bd6e6f862c70fa1bec9f8f2ef80719c89f4de
DogukanTopcu <dogukan_topcu3575@icloud.com>
2022-10-05 00:38:22 +0300

    discord community page

--- client/src/pages/Discord.js
+++ client/src/pages/Discord.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import "../styles/discord.scss";
 
+import discordImg from "../assets/images/discord.svg";
+
 const Discord = () => {
   return (
     <div className='discord'>
         <div className='container discord-content'>
-            <h1>You can come to my Discord community.</h1>
+            <img src={discordImg} className="discordImg"/>
+            <h1>You can come to my Discord Community :)</h1>
+            <button className='discordJoin'>Join Us</button>
         </div>
     </div>
   )
